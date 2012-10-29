85332b7e5bc0fd6946cf22656fe218665188a3a0
NickZhang <looloopig@gmail.com>
2012-10-29 10:49:46 +0800

    Commented for nonsense test

--- CodeParser/CodeParserTest.rb
+++ CodeParser/CodeParserTest.rb
@@ -3,6 +3,7 @@ require 'minitest/autorun'
 
 class CodeParserTest < MiniTest::Unit::TestCase
   
+  # Simple test here to show how to use MiniTest, but nonsense
   def testSimple
       codeParser = CodeParser.new()
       assert_equal 1, 1 
