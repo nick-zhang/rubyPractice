8878a7da35ffcdbf6036120980758d6697421dc8
NickZhang <looloopig@gmail.com>
2012-12-20 15:34:09 +0800

    Added constant variable

--- rubyLesson2/CaesarEncrypter.rb
+++ rubyLesson2/CaesarEncrypter.rb
@@ -1,12 +1,14 @@
 class CaesarEncrypter
+  private 
+  @@Z_UPPER_ASCII = 'Z'.ord
+  @@Z_LOWER_ASCII = 'z'.ord
+    
   public
   def encrypt text, step
     result = ''
-    z_upper_ord = 'Z'.ord 
-    z_lower_ord = 'z'.ord
     text.each_byte do |ascii|
       encryptedAscii = ascii + step
-      if  encryptedAscii > z_lower_ord || ( encryptedAscii > z_upper_ord and ascii <= z_upper_ord)
+      if  encryptedAscii > @@Z_LOWER_ASCII || ( encryptedAscii > @@Z_UPPER_ASCII and ascii <= @@Z_UPPER_ASCII)
         result += (encryptedAscii - 26).chr
       else
         result += encryptedAscii.chr
