d4f78bb5eff312bc06c54d90dcd244dd9b2d4c46
NickZhang <looloopig@gmail.com>
2012-12-25 15:16:48 +0800

    Using setup in test

--- rubyLesson2/SongTest.rb
+++ rubyLesson2/SongTest.rb
@@ -5,32 +5,34 @@ require 'minitest/autorun'
 
 class SongTest < MiniTest::Unit::TestCase
   
-    def testSongInitializeAndAddMoreFeaturesDynamically
-      song = Song.new("Happy Christmas!", "Nick", 30)
-      puts song.printName
-      puts song.printArtist
-      song.lyrics
-    end
+  def setup
+    @song = Song.new("Happy Christmas!", "Nick", 30)
+  end
+
+  def testSongInitializeAndAddMoreFeaturesDynamically
+    puts @song.printName
+    puts @song.printArtist
+    @song.lyrics
+  end
+
+  def testAccessSongAttributes
+    assert_equal "Happy Christmas!", @song.name  
+    assert_equal 30, @song.duration
+  end
     
-    def testAccessSongAttributes
-      song = Song.new("Happy Christmas!", "Nick", 30)
-      assert_equal "Happy Christmas!", song.name  
-      assert_equal 30, song.duration
-    end
-    
-    def testClassAttribute
-      song1 = Song.new("Happy Christmas!", "Nick", 30)
-      song2 = Song.new("One Love!", "Carry", 130)
-      
-      song1.play
-      song2.play
-    end
+  def testClassAttribute
+    song1 = Song.new("Happy Christmas!", "Nick", 30)
+    song2 = Song.new("One Love!", "Carry", 130)
   
+    song1.play
+    song2.play
+  end
+
   def testSongListClassMethod
     song1 = Song.new("One Love!", "Carry", 130)
     assert !(SongList.isTooLong song1)
     
-    song2 = Song.new("One Love!", "Carry", 350)
+    song2 = Song.new("One Love Again!", "James", 350)
     assert SongList.isTooLong song2    
   end
     
