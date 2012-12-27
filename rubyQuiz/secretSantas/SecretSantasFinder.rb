67caa0b65ab7bb24c0089e34cac48f421446f5d9
NickZhang <looloopig@gmail.com>
2012-12-27 14:23:00 +0800

    Deal with multiple players for secret santas

--- rubyQuiz/secretSantas/SecretSantasFinder.rb
+++ rubyQuiz/secretSantas/SecretSantasFinder.rb
@@ -1,13 +1,25 @@
 class SecretSantasFinder
 	
+	public 
 	def find players
-    mails = players.values.sort_by {rand}    
+	  originalMails = players.values
+    mails = players.values.sort_by {rand} 
+    mails = players.values.sort_by {rand} until compareMails originalMails, mails
+    
     i = 0
 	  players.each_key do |key|
       players[key] = mails[i]
       i+=1
     end 
 	  players
+  end
+  
+  private 
+  def compareMails originalMails, currentMails
+    originalMails.each_with_index do |mail, i|
+      return false if mail == currentMails[i]
+    end
+    return true
   end
   
 end
--- rubyQuiz/secretSantas/SecretSantasFinderTest.rb
+++ rubyQuiz/secretSantas/SecretSantasFinderTest.rb
@@ -12,6 +12,7 @@ class SecretSantasFinderTest < MiniTest::Unit::TestCase
               "Virgil Brigman"  => "<virgil@rigworkersunion.org>",
               "Lindsey Brigman" => "<lindsey@iseealiens.net>"
               }
+    @santasFinder = SecretSantasFinder.new()
   end
   
   def testShouldFindSantansForTwoPlayers
@@ -19,9 +20,19 @@ class SecretSantasFinderTest < MiniTest::Unit::TestCase
               "Leia Skywalker"  => "<leia@therebellion.org"
               }
     
-    santasFinder = SecretSantasFinder.new()
-    santas = santasFinder.find(players)
+    santas = @santasFinder.find(players)
     assert_equal "<luke@theforce.net>", santas["Leia Skywalker"]
   end
-
+  
+  def testShouldFindSantansForMultiplePlayers
+    santas = @santasFinder.find(@players)
+    refute_equal "<luke@theforce.net>",santas["Luke Skywalker"] 
+    refute_equal "<leia@therebellion.org>",santas["Leia Skywalker"] 
+    refute_equal "<toula@manhunter.org>",santas["Toula Portokalos"]
+    refute_equal "<gus@weareallfruit.net>",santas["Gus Portokalos"] 
+    refute_equal "<bruce@imbatman.com>",santas["Bruce Wayne"] 
+    refute_equal "<virgil@rigworkersunion.org>",santas["Virgil Brigman"] 
+    refute_equal "<lindsey@iseealiens.net>",santas["Lindsey Brigman"]      
+  end
+   
 end
